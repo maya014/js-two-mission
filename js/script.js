7886705a67db2413b473246500d340006d3ddc58
მაია კვარაცხელია <74621259+maya014@users.noreply.github.com>
2021-09-28 22:13:53 +0400

    ....

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 const date = new Date();
+
+const renderCalendar =() =>{
+  date.setDate(1);
+
+  const monthdays = document.querySelector(".days"); //მისამართი ჰტმლში
+ 
+ const lastDay = new Date(
+  date.getFullYear(),
+  date.getMonth() + 1,
+  0
+  ).getDate();
+
+ const prevLastDay = new Date(
+  date.getFullYear(),
+  date.getMonth(),
+  0
+  ).getDate();
+
+ const FirstDayindex = data.getDay();
+
+const lastDayindex =  new Date(
+  date.getFullYear(),
+  date.getMonth() + 1,
+   ).getDate();
+}
