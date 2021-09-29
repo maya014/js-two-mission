60c2237da3aad2f5763c8cc6c1ac2421f8504f02
მაია კვარაცხელია <74621259+maya014@users.noreply.github.com>
2021-09-29 15:28:06 +0400

    ....

--- js/script.js
+++ js/script.js
@@ -1,33 +1,33 @@
 const date = new Date();
 
-const renderCalendar =() => {
+const renderCalendar = () => {
   date.setDate(1);
 
-  const monthDays = document.querySelector(".days"); //მისამართი ჰტმლში
- 
- const lastDay = new Date(
-  date.getFullYear(),
-  date.getMonth() + 1,
-  0
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
   ).getDate();
 
- const prevLastDay = new Date(
-  date.getFullYear(),
-  date.getMonth(),
-  0
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
   ).getDate();
 
- const FirstDayIndex = data.getDay();
+  const firstDayIndex = date.getDay();
 
-const lastDayIndex =  new Date(
-  date.getFullYear(),
-  date.getMonth() + 1,
-  0
-   ).getDay();
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
 
-const nextDays = 7 - lastDayIndex - 1;
+  const nextDays = 7 - lastDayIndex - 1;
 
-const months =[
+  const months = [
     "January",
     "February",
     "March",
@@ -40,40 +40,41 @@ const months =[
     "October",
     "November",
     "December",
-];
+  ];
 
-document.querySelector(".data h1").innerHTML = months[data.getMonth()];
+  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 
-document.querySelector(".data p").innerHTML = new Date ().toDateString();
+  document.querySelector(".date p").innerHTML = new Date().toDateString();
 
-let days = "";
-  
+  let days = "";
 
-  for (let x =FirstDayIndex; x > 0; x--){
+  for (let x = firstDayIndex; x > 0; x--) {
     days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
   }
-  for(let i = 1; i <= lastDay; i++){
-    if(
-      i === new date().getDate() && 
-      date.getMonth() === new date().getMonth()
-      ){
-      days +=`<div class="today">${i}</div`;
-    }else{
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
+      days += `<div class="today">${i}</div>`;
+    } else {
       days += `<div>${i}</div>`;
     }
   }
-  for(let j = 1; j <= nextDays; j++){
-    days +=`<div class="next-date">${j}<div>`;
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
     monthDays.innerHTML = days;
   }
 };
 
-document.querySelector(".prev").addEventListener("click",() =>{
+document.querySelector(".prev").addEventListener("click", () => {
   date.setMonth(date.getMonth() - 1);
   renderCalendar();
 });
 
-document.querySelector(".next").addEventListener("click",() => {
+document.querySelector(".next").addEventListener("click", () => {
   date.setMonth(date.getMonth() + 1);
   renderCalendar();
 });
